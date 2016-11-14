505caee432c7f7ce38cd916b1e9a4be50d5bfa7d
Matej Marinko <matejmarinko123@gmail.com>
2016-11-14 09:20:59 +0100

    Gradientni spust za 10000 tock.

--- minimiziranje_napake.py
+++ minimiziranje_napake.py
@@ -5,9 +5,25 @@ with open('meritve.txt') as f:
 
 data = [[float(i.split()[0]), float(i.split()[1])] for i in data]
 
-a = numpy.array([[i[0], 1] for i in data])
-b = numpy.array([i[1] for i in data])
+# a = numpy.array([[i[0], 1] for i in data])
+# b = numpy.array([i[1] for i in data])
+#
+# c = numpy.linalg.lstsq(a, b)[0]
+#
+# print(c)
 
-c = numpy.linalg.lstsq(a, b)[0]
+def gradientni_spust(data, a, b, grad_a, grad_b, step):
+    a -= step * grad_a(a, b, data)
+    b -= step * grad_b(a, b, data)
+    return a, b
 
-print(c)
+grad_a = lambda a, b, l: sum([-x * (y - a * x - b) for x, y in l])
+grad_b = lambda a, b, l: sum([-(y - a * x - b) for x, y in l])
+
+a, b = 0, 0
+for i in range(50000):
+    a, b = gradientni_spust(data, a, b, grad_a, grad_b, 5e-8)
+    if i % 100 == 0: print(a, b)
+print(a, b)
+#-1.2984640721985186 2.190221283340356
+
