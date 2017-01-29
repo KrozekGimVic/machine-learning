542648d983175364d3b5eff004e91b3243fc774d
Vid Drobnic <vid.drobnic@gmail.com>
2017-01-29 16:53:28 +0100

    added server base

--- README.md
+++ README.md
@@ -4,4 +4,6 @@ Learning machine learning.
 ## Project Structure
 - ```Learning``` directory has mostly usless stuff, that was written to learn the concepts behind the digits clasifier.
 - ```old_main.py``` is a bad try at writing the digits clasifier. It has no mathematical opimizations therefore it's slow and useless.
-- ```skynet.py``` is library that will be later used for creating interactive console application for digit classifying.
+- ```skynet.py``` is library that will be later used in application for digit classifying.
+- ```server.py``` is a simple server that uses ```skynet.py``` to accept images and use them for learning.
+Later it will be able to server engine.csv or recognize image by uploading it to the server
---
+++ server.py
@@ -0,0 +1,37 @@
+from http.server import HTTPServer, BaseHTTPRequestHandler
+from urllib import parse
+import json
+
+class Server(BaseHTTPRequestHandler):
+    def _set_headers(self):
+        self.send_response(200)
+        self.send_header('Content-type', 'application/json')
+        self.end_headers()
+
+    def do_GET(self):
+        self._set_headers()
+        self.wfile.write('Not implemented yet.\n'.encode('utf-8'))
+
+    def do_POST(self):
+        self._set_headers()
+        if self.path != '/upload_image':
+            self.wfile.write('Wrong url.\n'.encode('utf-8'))
+            return
+
+        try:
+            content_len = int(self.headers['content-length'])
+            data = self.rfile.read(content_len)
+            data_dict = json.loads(data)
+
+            label = data_dict['label']
+            image = data_dict['image']
+            print(label, image)
+        except:
+            self.wfile.write('Wrong json data.\n'.encode('utf-8'))
+
+def run():
+    server_address = ('', 8000)
+    httpd = HTTPServer(server_address, Server)
+    httpd.serve_forever()
+
+run()
