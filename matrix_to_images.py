1e4aec25c431ff99f3e659879ca8cd320adf1871
Matej Marinko <matejmarinko123@gmail.com>
2017-01-30 08:52:54 +0100

    Bux fixes.

--- matrix_to_images.py
+++ matrix_to_images.py
@@ -2,18 +2,18 @@ from PIL import Image
 
 with open('engine.csv') as f:
     l = f.readlines()
-    l = l[0].split(',')
+    l = [i.split(',') for i in l]
 
-new_image = Image.new('L', (28, 28))
+new_image = Image.new('L', (28 * 10, 28))
 new_image_data = new_image.getdata()
 
+l = [[float(i) for i in j] for j in l]
+max_val = max([max(i) for i in l])
 
-l = [float(i) for i in l]
-max_val = max(l)
+for _ in range(10):
+    for i in range(28):
+        for j in range(28):
+            val = int(l[_][28 * i + j] * 256 / max_val)
+            new_image_data.putpixel((_*28 + i, j), (val))
 
-for i in range(28):
-    for j in range(28):
-        val = int(l[28 * i + j] * 256 / max_val)
-        new_image_data.putpixel((i, j), (val))
-
-new_image.save('image.bmp')
+new_image.save('image.png')
