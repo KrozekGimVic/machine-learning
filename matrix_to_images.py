c885bb45d3eb145b86eca7885eaaf12b8af37f25
Matej Marinko <matejmarinko123@gmail.com>
2017-01-31 16:49:26 +0100

    matrix visulalisation, different color for negative weights

--- matrix_to_images.py
+++ matrix_to_images.py
@@ -4,16 +4,21 @@ with open('engine.csv') as f:
     l = f.readlines()
     l = [i.split(',') for i in l]
 
-new_image = Image.new('L', (28, 28 * 10))
+new_image = Image.new('RGB', (28, 28 * 10))
 new_image_data = new_image.getdata()
 
 l = [[float(i) for i in j] for j in l]
-max_val = max([max(i) for i in l])
+max_val = max([max(i, key=abs) for i in l])
 
 for _ in range(10):
     for i in range(28):
         for j in range(28):
             val = int(l[_][i + 28 * j] * 256 / max_val)
-            new_image_data.putpixel((i, j + 28*_), (val))
+            if val < 0:
+                color = (-val, 0, 0)
+            else:
+                color = (0, val, 0)
+            print(val)
+            new_image_data.putpixel((i, j + 28*_), color)
 
 new_image.save('image.png')
